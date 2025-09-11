6567fef2c22e62af272fe71e6f766901d8e6322a
Niviane Alves <nivianecriativa@gmail.com>
2025-09-11 19:07:49 -0300

    Prática 01 – Parte 3

--- app/src/main/java/com/weatherappniviane/MainActivity.kt
+++ app/src/main/java/com/weatherappniviane/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.weatherappniviane//package com.weatherappniviane
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.weatherappniviane.ui.theme.WeatherAppNivianeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +30,47 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherAppNivianeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomePage(modifier = Modifier.padding(innerPadding))
+
+
                 }
             }
         }
     }
 }
 
+@SuppressLint("ContextCastToActivity")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun HomePage(modifier: Modifier = Modifier) {
+    val activity = LocalContext.current as? Activity
+
+    Column(
         modifier = modifier
-    )
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-vindo à Home!",
+            fontSize = 24.sp
+        )
+        Button(
+            onClick = {
+                activity?.finish()
+            },
+            modifier = Modifier
+                .padding(top = 16.dp)
+        ) {
+            Text(text = "Sair")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomePagePreview() {
     WeatherAppNivianeTheme {
-        Greeting("Android")
+        HomePage()
     }
 }
